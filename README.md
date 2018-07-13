515ba9640bd3da2d85a5c04d84c73c2eede5ab1e
Jiahyemail <Jiahyemail@163.com>
2018-07-13 11:07:25 +0800

    修改返回形式

--- README.md
+++ README.md
@@ -2,28 +2,35 @@
 
 ##项目简介
 #####HyAliapyApi主要用于支付宝账户的账单查询与账单文件下载，此API一共三个方法！
->1、根据时间范围或默认时间范围下载文件，若不设定时间，默认下载当天账单。<br/>
->2、根据交易流水号或商户订单号或时间范围查询对应订单信息。若不设定时间，默认查询为以当前时间一周之前的账单。<br/>
->3、查询账户余额。<br/>
+>1、根据时间范围或默认时间范围下载文件，若不设定时间，默认下载当天账单。
+>2、根据交易流水号或商户订单号或时间范围查询对应订单信息。若不设定时间，默认查询为以当前时间一周之前的账单。
+>3、查询账户余额。
 
 ##安装方式：python setup.py install
 
 ##使用实例
+
 ```python
+
 from hy_alipay_sdk import aliapy_api
 
 class AlipayApi(object):
-    def __init__(self, account, alipay_id, cookies, filename=None):
+    def __init__(self, account, alipay_id, cookies,proxies_url,proxies_seller, filename=None):
         '''
         :param account:账号
         :param alipay_id: 账号ID
         :param cookies: cookies
+        :param proxies_url: 代理地址
+        :param proxies_seller: 代理类型
+        
         :param filename: 下载文件时需要传入路径，给文件一个临时存储位置，最后读取文件并删除！
         '''
         self.account = account
         self.alipay_id = alipay_id
         self.cookies = cookies
         self.filename = filename    
+        self.proxies_url = proxies_url    
+        self.proxies_seller = proxies_seller    
         
     def download_file(proxies, start_time=None, end_time=None, input_charset='gbk'):
         '''
@@ -55,67 +62,83 @@ class AlipayApi(object):
         return money
     
 if __name__ == '__main__':
-    aliapy_api.AlipayApi(account, alipay_id, cookies,'c:/hy_alipay_sdk/').download_file(proxies,'2018-07-10','2018-07-11','gbk')
-    aliapy_api.AlipayApi(account, alipay_id, cookies).search_by_trade_no_or_trans_id(proxies,trade_no,'2018-07-10','2018-07-11')
-    aliapy_api.AlipayApi(account, alipay_id, cookies).get_balance(proxies)
+    aliapy_api.AlipayApi(account, alipay_id, cookies,proxies_url,proxies_seller,'c:/hy_alipay_sdk/').download_file(proxies,'2018-07-10','2018-07-11','gbk')
+    aliapy_api.AlipayApi(account, alipay_id, cookies,proxies_url,proxies_seller).search_by_trade_no_or_trans_id(proxies,trade_no,'2018-07-10','2018-07-11')
+    aliapy_api.AlipayApi(account, alipay_id, cookies,proxies_url,proxies_seller).get_balance(proxies)
     
 ```        
-##ERROR
->1、003-访问被拒绝<br/>
->2、004-访问超时<br/>
->3、005-Cannot connect to proxy<br/>
-以上属于连接是发生的错误，程序会进行重试，在重试三次若无效，程序会停止运行。应检查代理IP是否有效，网站结构是否更改！<br/>
->4、001-下载压缩包失败,请检查<br/>
->5、002-压缩包读取失败，请检查<br/>
-检查文件是否下载，下载后的ZIP是否为正常文件<br/>
 
-#return
-####download_file<br/>
-  >>return ["#账号：tbpek0501@163.com[20882218414870830156]\n", "#查询起始日期：2018-07-11 00:00:00查询终止日期：2018-07-12 00:00:00\n"]<br/>
-  若失败则 return [] or 001 or 002 error
 
-####search_by_trans_id_or_trade_no<br/>
-  >>return [
-  {
-    'created': 创建时间，
-    'trade_no': 订单号,
-    'business_serial_number': 交易号,
-    'product_name': 名称,
-    'income_amount': 金额,
-    'counterparty': 对方,
-    'status': 状态
-  }
-  ]
-  若失败则 return[] or 003 or 004 or 005
+#返回参数处理
+
+## 通用异常返回结果
+
+```
+ {
+                "apiVersion": "1.0",
+                "error": {
+                    "message": '输入参数为空',
+                    "code": 10001,
+                    "errors": 'null'
+                }
+            }
+```
+
+####download_file
+```
+{
+    "file_content": [#账号：tbpek0501@163.com[20882218414870830156]\n", "#查询起始日期：2018-07-12 00:00:00查询终止日期：2018-07-13 00:00:00\n]
+        
+}
+```
+
+####search_by_trans_id_or_trade_no
+```
+{
+    "content": [{
+        "trade_no": "订单号",
+        "business_serial_number": " 交易号",
+        "amount": "金额",
+        "counterparty": "对方",
+        "created": "创建时间",
+        "record_type_desc": "状态",
+        "product_name": "",
+        "accounts_serial_number": "交易号"
+    }]
+}
+```
   
-
+  
 ####get_balance
-  >>return float(balance) or None
+
+```
+{'money': '173967.59'}
+```
 
 
 ##项目结构
-hy_alipay_sdk:<br/>
-│<br/>
-│<br/>
-│  MANIFEST.in<br/>
-│  README<br/>
-│  README.md<br/>
-│  requirements.txt<br/>
-│  setup.cfg<br/>
-│  setup.py<br/>
-│  test-requirements.txt<br/>
-│  tox.ini<br/>
-│  <br/>
-│     <br/> 
-├─hy_alipay_sdk<br/>
-│      aliapy_api.py<br/>
-│      headers.py<br/>
-│      httpclient.py<br/>
-│      utils.py<br/>
-│      zipfileadd.py<br/>
-│      __init__.py<br/>
-│      <br/>
+hy_alipay_sdk:
+│
+│
+│  MANIFEST.in
+│  README
+│  README.md
+│  requirements.txt
+│  setup.cfg
+│  setup.py
+│  test-requirements.txt
+│  tox.ini
+│  
+│      
+├─hy_alipay_sdk
+│      aliapy_api.py
+│      headers.py
+│      httpclient.py
+│      utils.py
+│      zipfileadd.py
+│      __init__.py
+│      
 └─tests
-        cli.py<br/>
-        _test.py<br/>
-        __init__.py<br/>
+        cli.py
+        _test.py
+        __init__.py
--- hy_alipay_sdk/aliapy_api.py
+++ hy_alipay_sdk/aliapy_api.py
@@ -14,64 +14,76 @@ from hy_alipay_sdk.httpclient import HTTPClient
 from hy_alipay_sdk.headers import *
 from urllib3.exceptions import InsecureRequestWarning
 
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 t = time.time()
 nowTime = lambda: int(round(t * 1000))
 SEARCH_URL = 'https://consumeprod.alipay.com/record/advanced.htm'
 DOWN_FILE_JSON_URL = "https://mbillexprod.alipay.com/enterprise/entAsyncSecurityCheck.json"
 DOWN_ZIP_FILE_URL = "https://mbillexprod.alipay.com/enterprise/fundAccountDetailSyncDownload.resource?_t={0}&queryEntrance=1&billUserId=2088221841487083&showType=0&type=&precisionQueryKey=tradeNo&startDateInput={1}%2000:00:00&endDateInput={2}%2000:00:00&forceAync=0&downloadType=CSV&securityBizType=SECURITY_PRODUCT_ACCOUNT&securityId={3}"
-PROXIES_URL = 'http://114.215.45.93:60018'
-PROXIES_SELLER = 'yh'
+
 
 
 class AlipayApi(object):
-    def __init__(self, account, alipay_id, cookies, filename=None):
+    def __init__(self, account, alipay_id, cookies, proxies_url,proxies_seller,filename=None):
         self.account = account
         self.alipay_id = alipay_id
         self.cookies = cookies
         self.filename = filename
+        self.proxies_url=proxies_url
+        self.proxies_seller=proxies_seller
 
     def download_file(self, proxies, start_time=None, end_time=None, input_charset='gbk'):
-        '''
 
+        '''
         :param proxies: 代理
         :param start_time: 开始时间
         :param end_time: 结束时间
         :param input_charset: 编码方式
 
         '''
-        # 设定默认时间
-        START_TIME = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min).strftime(
-            '%Y-%m-%d')
-        END_TIME = datetime.datetime.combine(
-            datetime.date.today() - datetime.timedelta(days=-1),
-            datetime.time.min).strftime(
-            '%Y-%m-%d')
-        # 如果未传入时间参数则使用默认时间，否则使用传入的时间参数
-        if start_time == None or end_time == None:
-            start_time = START_TIME
-            end_time = END_TIME
-        else:
-            start_time = start_time
-            end_time = end_time
+        try:
+            # 设定默认时间
+            START_TIME = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min).strftime(
+                '%Y-%m-%d')
+            END_TIME = datetime.datetime.combine(
+                datetime.date.today() - datetime.timedelta(days=-1),
+                datetime.time.min).strftime(
+                '%Y-%m-%d')
+            # 如果未传入时间参数则使用默认时间，否则使用传入的时间参数
+            if start_time == None or end_time == None:
+                start_time = START_TIME
+                end_time = END_TIME
+            else:
+                start_time = start_time
+                end_time = end_time
 
-        DOWN_FILE_JSON_DATA = {'securityBizType': 'SECURITY_PRODUCT_ACCOUNT',
-                               'beginDate': start_time,
-                               'endDate': end_time,
-                               '_input_charset': input_charset
-                               }
-        # 初始化对象
-        INIT = HTTPClient(PROXIES_URL, PROXIES_SELLER, self.cookies, proxies)
-        # 请求下载之前的JSON地址得到securityId
-        req_down_file_json = INIT.request('POST', DOWN_FILE_JSON_URL, data=DOWN_FILE_JSON_DATA,
-                                          headers=DOWN_FILE_JSON_HEADERS)
+            DOWN_FILE_JSON_DATA = {'securityBizType': 'SECURITY_PRODUCT_ACCOUNT',
+                                   'beginDate': start_time,
+                                   'endDate': end_time,
+                                   '_input_charset': input_charset
+                                   }
+            # 初始化对象
+            INIT = HTTPClient(self.proxies_url, self.proxies_seller, self.cookies, proxies)
+            # 请求下载之前的JSON地址得到securityId
+            req_down_file_json = INIT.request('POST', DOWN_FILE_JSON_URL, data=DOWN_FILE_JSON_DATA,
+                                              headers=DOWN_FILE_JSON_HEADERS)
 
-        # 下载文件
-        down_file = INIT.request('GET', DOWN_ZIP_FILE_URL.format(nowTime(), start_time, end_time,
-                                                                 req_down_file_json.json()['securityId']), headers=DOWN_FILE_HEADERS)
-        self.down_zip_file(down_file.content)
-
-        return self.unzip_file()
+            # 下载文件
+            down_file = INIT.request('GET', DOWN_ZIP_FILE_URL.format(nowTime(), start_time, end_time,
+                                                                     req_down_file_json.json()['securityId']), headers=DOWN_FILE_HEADERS)
+            self.down_zip_file(down_file.content)
+            dict={'file_content':self.unzip_file()}
+            return json.dumps(dict, encoding="UTF-8", ensure_ascii=False, sort_keys=False, indent=4)
+        except Exception as e:
+            return {
+                    "apiVersion": "1.0",
+                    "error": {
+                        "message": e,
+                        "code": 10001,
+                        "errors": 'null'
+                    }
+                }
 
     def search_by_trade_no_or_trans_id(self, proxies, trade_no, start_time=None, end_time=None):
         '''
@@ -81,43 +93,55 @@ class AlipayApi(object):
         :param end_time: 结束时间
         :return:parse content
         '''
-        START_TIME = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min).strftime('%Y.%m.%d')
-        END_TIME = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=6),
-                                             datetime.time.min).strftime('%Y.%m.%d')
-        if start_time == None or end_time == None:
-            start_date = START_TIME
-            end_date = END_TIME
-        else:
-            timeTuple = time.strptime(start_time, "%Y-%m-%d")
-            timeTuple_end = time.strptime(end_time, "%Y-%m-%d")
-            start_date = time.strftime("%Y.%m.%d", timeTuple)
-            end_date = time.strftime("%Y.%m.%d", timeTuple_end)
-        SEARCH_DATA = {
-            'rdsToken': '',
-            'rdsUa': '',
-            'beginDate': start_date,
-            'beginTime': '00:00',
-            'endDate': end_date,
-            'endTime': '24:00',
-            'dateRange': 'sevenDays',
-            'status': 'all',
-            'keyword': 'bizNo',
-            'keyValue': trade_no,
-            'dateType': 'createDate',
-            'minAmount': '',
-            'maxAmount': '',
-            'fundFlow': 'all',
-            'tradeType': 'ALL',
-            'categoryId': '',
-            'pageNum': '1'
-        }
+        try:
+            START_TIME = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min).strftime('%Y.%m.%d')
+            END_TIME = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=6),
+                                                 datetime.time.min).strftime('%Y.%m.%d')
+            if start_time == None or end_time == None:
+                start_date = START_TIME
+                end_date = END_TIME
+            else:
+                timeTuple = time.strptime(start_time, "%Y-%m-%d")
+                timeTuple_end = time.strptime(end_time, "%Y-%m-%d")
+                start_date = time.strftime("%Y.%m.%d", timeTuple)
+                end_date = time.strftime("%Y.%m.%d", timeTuple_end)
+            SEARCH_DATA = {
+                'rdsToken': '',
+                'rdsUa': '',
+                'beginDate': start_date,
+                'beginTime': '00:00',
+                'endDate': end_date,
+                'endTime': '24:00',
+                'dateRange': 'sevenDays',
+                'status': 'all',
+                'keyword': 'bizNo',
+                'keyValue': trade_no,
+                'dateType': 'createDate',
+                'minAmount': '',
+                'maxAmount': '',
+                'fundFlow': 'all',
+                'tradeType': 'ALL',
+                'categoryId': '',
+                'pageNum': '1'
+            }
 
-        get_total = HTTPClient(PROXIES_URL, PROXIES_SELLER, self.cookies, proxies).request('POST',
-                                                                                           SEARCH_URL,
-                                                                                           data=SEARCH_DATA,
-                                                                                           headers=SEARCH_HEADERS)
+            get_total = HTTPClient(self.proxies_url, self.proxies_seller, self.cookies, proxies).request('POST',
+                                                                                               SEARCH_URL,
+                                                                                               data=SEARCH_DATA,
+                                                                                               headers=SEARCH_HEADERS)
+            dict={'content':self.spider(get_total)}
 
-        return self.spider(get_total)
+
+            return json.dumps(dict, encoding="UTF-8", ensure_ascii=False, sort_keys=False)
+        except Exception as e:
+            return {
+                "apiVersion": "1.0",
+                "error": {
+                    "message": e,
+                    "code": 10001,
+                    "errors": 'null'
+                }
+            }
 
     def spider(self, get_total):
         '''
@@ -125,77 +149,105 @@ class AlipayApi(object):
         :param get_total: response
         :return: content
         '''
-        response = etree.HTML(get_total.text)
-        created_data = response.xpath('//p[@class="time-d"]/text()')
-        created_time = response.xpath('//p[@class="time-h ft-gray"]/text()')
-        trade_nos = response.xpath('//td[@class="tradeNo ft-gray"]/p/text()')
-        product_name = response.xpath('//p[@class="consume-title"]/a/text()')
-        amount = response.xpath('//td[@class="amount"]/span/text()')
-        counterparty = response.xpath('//p[@class="name"]/text()')
-        record_type_desc = response.xpath('//td[@class="status"]/p/text()')
-        content = []
-        for i in range(0, len(trade_nos)):
-            dict = {
-                'created': re.sub('\t|\n|\r|', '', created_data[i] + ' ' + created_time[i]),
-                'trade_no': trade_nos[i].split('|')[0],
-                'business_serial_number': trade_nos[i].split('|')[1].split('_')[0],
-                'product_name': product_name[i],
-                'amount': ''.join(amount[i]),
-                'counterparty': re.sub('\t|\n|\r|', '', counterparty[i]),
-                'record_type_desc': record_type_desc[i],
-                'accounts_serial_number': trade_nos[i].split('|')[1]
+        try:
+            response = etree.HTML(get_total.text)
+            created_data = response.xpath('//p[@class="time-d"]/text()')
+            created_time = response.xpath('//p[@class="time-h ft-gray"]/text()')
+            trade_nos = response.xpath('//td[@class="tradeNo ft-gray"]/p/text()')
+            product_name = response.xpath('//p[@class="consume-title"]/a/text()')
+            amount = response.xpath('//td[@class="amount"]/span/text()')
+            counterparty = response.xpath('//p[@class="name"]/text()')
+            record_type_desc = response.xpath('//td[@class="status"]/p/text()')
+            content = []
+            for i in range(0, len(trade_nos)):
+                dict = {
+                    'created': re.sub('\t|\n|\r|', '', created_data[i] + ' ' + created_time[i]),
+                    'trade_no': trade_nos[i].split('|')[0],
+                    'business_serial_number': trade_nos[i].split('|')[1].split('_')[0],
+                    'product_name': product_name[i],
+                    'amount': ''.join(amount[i]),
+                    'counterparty': re.sub('\t|\n|\r|', '', counterparty[i]),
+                    'record_type_desc': record_type_desc[i],
+                    'accounts_serial_number': trade_nos[i].split('|')[1]
+                }
+                content.append(dict)
+            return content
+        except Exception as e:
+            return {
+                "apiVersion": "1.0",
+                "error": {
+                    "message": e,
+                    "code": 10001,
+                    "errors": 'null'
+                }
             }
-            content.append(dict)
-        return json.dumps(content, encoding="UTF-8", ensure_ascii=False)
-
     def get_balance(self, proxies):
         '''
         Get balance
         :param proxies: proxies
         :return: money
         '''
-        START_TIME = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min).strftime('%Y.%m.%d')
-        END_TIME = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=6),
-                                             datetime.time.min).strftime('%Y.%m.%d')
-        SEARCH_DATA = {
-            'rdsToken': '',
-            'rdsUa': '',
-            'beginDate': START_TIME,
-            'beginTime': '00:00',
-            'endDate': END_TIME,
-            'endTime': '24:00',
-            'dateRange': 'sevenDays',
-            'status': 'all',
-            'keyword': 'bizNo',
-            'keyValue': '',
-            'dateType': 'createDate',
-            'minAmount': '',
-            'maxAmount': '',
-            'fundFlow': 'all',
-            'tradeType': 'ALL',
-            'categoryId': '',
-            'pageNum': '1'
-        }
-        get_total = HTTPClient(PROXIES_URL, PROXIES_SELLER, self.cookies, proxies).request('POST',
-                                                                                           SEARCH_URL,
-                                                                                           data=SEARCH_DATA,
-                                                                                           headers=SEARCH_HEADERS)
+        try:
+            START_TIME = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min).strftime('%Y.%m.%d')
+            END_TIME = datetime.datetime.combine(datetime.date.today() - datetime.timedelta(days=6),
+                                                 datetime.time.min).strftime('%Y.%m.%d')
+            SEARCH_DATA = {
+                'rdsToken': '',
+                'rdsUa': '',
+                'beginDate': START_TIME,
+                'beginTime': '00:00',
+                'endDate': END_TIME,
+                'endTime': '24:00',
+                'dateRange': 'sevenDays',
+                'status': 'all',
+                'keyword': 'bizNo',
+                'keyValue': '',
+                'dateType': 'createDate',
+                'minAmount': '',
+                'maxAmount': '',
+                'fundFlow': 'all',
+                'tradeType': 'ALL',
+                'categoryId': '',
+                'pageNum': '1'
+            }
+            get_total = HTTPClient(self.proxies_url, self.proxies_seller, self.cookies, proxies).request('POST',
+                                                                                               SEARCH_URL,
+                                                                                               data=SEARCH_DATA,
+                                                                                               headers=SEARCH_HEADERS)
 
-        response = etree.HTML(get_total.text)
+            response = etree.HTML(get_total.text)
 
-        money = response.xpath('//em[@class="ft-green"]/strong/text()')
-        return ''.join(money)
+            money = response.xpath('//em[@class="ft-green"]/strong/text()')
+            return {'money':''.join(money)}
+        except Exception as e:
+            return {
+                "apiVersion": "1.0",
+                "error": {
+                    "message": e,
+                    "code": 10001,
+                    "errors": 'null'
+                }
+            }
+
 
     def down_zip_file(self, content):
         '''
         :param content:file content
         '''
+
         try:
             with open(self.filename + str(self.alipay_id) + str('.zip'), 'wb')as f:
                 f.write(content)
                 f.close()
         except Exception as e:
-            logging.error('001-下载压缩包失败,请检查%s' % e)
+            return {
+                "apiVersion": "1.0",
+                "error": {
+                    "message": e,
+                    "code": 10001,
+                    "errors": 'null'
+                }
+            }
 
     def unzip_file(self):
 
@@ -203,22 +255,33 @@ class AlipayApi(object):
         decompression.
 
         '''
-        content = []
         try:
-            zip_file_path = self.filename + str(self.alipay_id) + str('.zip')
-            zfile = zipfileadd.ZipFile(zip_file_path, 'r')
-            for csv_file_name in zfile.namelist():
-                zfile.extract(csv_file_name, self.filename)
-                filename_path = unicode(self.filename + csv_file_name.encode('utf-8'), "utf8")
-                f = codecs.open(filename_path, 'r', 'gbk')
-                text = f.readlines()
-                for i in text:
-                    content.append(i)
-                f.close()
-                zfile.close()
-                os.remove(filename_path)
-                os.remove(zip_file_path)
+            content = []
+            try:
+                zip_file_path = self.filename + str(self.alipay_id) + str('.zip')
+                zfile = zipfileadd.ZipFile(zip_file_path, 'r')
+                for csv_file_name in zfile.namelist():
+                    zfile.extract(csv_file_name, self.filename)
+                    filename_path = unicode(self.filename + csv_file_name.encode('utf-8'), "utf8")
+                    f = codecs.open(filename_path, 'r', 'gbk')
+                    text = f.readlines()
+                    for i in text:
+                        content.append(i)
+                    f.close()
+                    zfile.close()
+                    os.remove(filename_path)
+                    os.remove(zip_file_path)
 
+            except Exception as e:
+                logging.error("002-压缩包读取失败，请检查%s" % e)
+            return content
         except Exception as e:
-            logging.error("002-压缩包读取失败，请检查%s" % e)
-        return json.dumps(content, encoding="UTF-8", ensure_ascii=False)
+            return {
+                "apiVersion": "1.0",
+                "error": {
+                    "message": e,
+                    "code": 10001,
+                    "errors": 'null'
+                }
+            }
+
